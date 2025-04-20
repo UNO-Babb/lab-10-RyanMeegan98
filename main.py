72563b5ee1753bf31ce5d0c3243be7d5b2d1ba41
RyanMeegan98 <ryan.meegan1171@gmail.com>
2025-04-20 18:47:18 +0000

    Completed

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 #MapPlot.py
-#Name:
-#Date:
-#Assignment:
+#Name:Ryan Meegan
+#Date:Apr 20 2014
+#Assignment:Lab 10
+
+import medal_of_honor
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = medal_of_honor.get_awardee()
+
+organizations = []
+for person in data:
+    if "military record" in person:
+        record = person["military record"]
+        if "organization" in record and record["organization"] != "":
+            organizations.append(record["organization"])
+
+counts = pd.Series(organizations).value_counts()
+
+plt.bar(counts.index, counts.values, color='skyblue')
+plt.title("Number of Medal of Honor Recipients by Military Branch")
+plt.xlabel("Military Service Branch")
+plt.ylabel("Number of Recipients")
+plt.xticks(rotation=45)
+
+plt.tight_layout()
+plt.savefig("medal_of_honor_service_chart.png")
+plt.show()
